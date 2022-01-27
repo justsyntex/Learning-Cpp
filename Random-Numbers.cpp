5b0166a30cf25d0c0d851fcdc03e067fb531d70d
Vyacheslav <dalealrussia@gmail.com>
2022-01-27 04:00:11 +0300

    Update Random-Numbers.cpp

--- Random-Numbers.cpp
+++ Random-Numbers.cpp
@@ -9,27 +9,27 @@ int main()
 {
     srand(time(NULL));
 
-	const short SIZE = 10;
-	short a[SIZE];
+    const short SIZE = 10;
+    short a[SIZE];
 
-	for (short i = 0; i < SIZE; i++)
-	{
-		a[i] = rand() % 20;
+    for (short i = 0; i < SIZE; i++)
+    {
+        a[i] = rand() % 20;
 
-		for (short t = 0; t < i; t++)
-		{
-			if (a[i] == a[t])
-			{
-				i--;
-				break;
-			}
-		}
-	}
+        for (short t = 0; t < i; t++)
+        {
+            if (a[i] == a[t])
+            {
+                i--;
+                break;
+            }
+        }
+    }
 
-	for (short i = 0; i < SIZE; i++)
-	{
-		cout << a[i] << endl;
-	}
+    for (short i = 0; i < SIZE; i++)
+    {
+        cout << a[i] << endl;
+    }
 
     system("pause");
 }
